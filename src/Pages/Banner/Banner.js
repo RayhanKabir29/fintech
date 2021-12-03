6d639b860505ada287ee36eb6a4ca3e23bbdbd04
RayhanKabir29 <rayhan.kabir29@gmail.com>
2021-12-03 12:54:30 +0600

    banner add

---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="banner-img">
+                <h2>Bannaer</h2>
+                <img src="https://ibb.co/3RT1HSj" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <>
+            <Navbar bg="light" variant="light">
+                <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="ms-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Pages</Nav.Link>
+                <Nav.Link href="#pricing">Services</Nav.Link>
+                <Nav.Link href="#home">Shop</Nav.Link>
+                <Nav.Link href="#features">Blog</Nav.Link>
+                <Nav.Link href="#pricing">Contact</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+         </>
     );
 };
 
