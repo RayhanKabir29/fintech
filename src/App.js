9197a93cc0fecbd83e40d3b62dd08edf65a4ea87
RayhanKabir29 <rayhan.kabir29@gmail.com>
2021-12-04 12:19:53 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Shared/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Home/>
     </div>
   );
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row,Button } from 'react-bootstrap';
 import './Services.css';
 
 const Services = () => {
     return (
         <div>
-            <Container>
+            <Container className="mt-5">
                 <Row>
                     <Col xs={12} md={6} className= "text-center text-sm-left" >
                         <div className="section-header">
@@ -18,9 +18,48 @@ const Services = () => {
                             <p>
                             It's not just about your money, it's about your life. Finvision professionals understand how complex your life and financial situation can be, and we're here to help. Our team of certified financial planners can help you get the right information so you can make the best decisions.
                             </p>
+                            <Button>Our Services</Button>
                         </div>
+                        
                     </Col>
                     <Col xs={12} md={6}>
+                        <Row className="mb-5">
+                            <Col xs={12} md={6}>
+                                <Card className="mb-3 h-100">
+                                    <Card.Img variant="top" src="" />
+                                    <Card.Body>
+                                        <Card.Title>Financial Planing</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card className="mb-3 h-100">
+                                    <Card.Img variant="top" src="" />
+                                    <Card.Body>
+                                        <Card.Title>Investment Planing</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Row >
+                        <Col xs={12} md={6}>
+                                <Card className="mb-3 h-100">
+                                    <Card.Img variant="top" src="" />
+                                    <Card.Body>
+                                        <Card.Title>Retirement Planing</Card.Title>
+                                        
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Card className="mb-3 h-100">
+                                    <Card.Img variant="top" src="" />
+                                    <Card.Body>
+                                        <Card.Title>Devorce Planing</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
