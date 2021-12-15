5e6680eed060215585bd59a936443f9fff5673cb
RayhanKabir29 <rayhan.kabir29@gmail.com>
2021-12-16 00:35:12 +0600

    banner

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import CPA from '../../images/CPA.jpg';
+import TAB from '../../images/Tpb-Logopng.png';
 import './Banner.css';
 
 const Banner = () => {
     return (
       <div>
        <div className="banner-section">
-         <Container>
+         <Container className='banner py-3'>
            <Row>
-            <Col md={4} xs={12}>
-              <h1>Image</h1>
+            <Col md={4} sm={12}>
+             <div className="banner-img d-flex">
+              <img src={TAB} alt="" className="img-fluid" style={{width:"130px", height:"105px"}}/>
+              <img src={CPA} alt=""  className="img-fluid" style={{width:"200px", height:"100px", marginLeft:"10px"}}/>
+             </div>
             </Col>
-            <Col md={8} xs={12}>
-              <p>text</p>
+            <Col md={8} xs={12} className="">
+              <Row>
+                <h2>Register Now for NSW COVID19 Business Grants</h2>
+              </Row>
+              <Row>
+                <div className="banner-btn mt-3 d-flex">
+                <Col md={4}>
+                  <Button>Register Your Interest</Button>
+                </Col>
+                <Col md={4}>
+                  <Button>Easy Tax Return</Button>
+                </Col>
+                </div>
+              </Row>
             </Col>
            </Row>
          </Container>
