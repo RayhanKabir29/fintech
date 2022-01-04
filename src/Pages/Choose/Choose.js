e407d32711404edc80d2f2d7e8053aa7b08b408a
RayhanKabir29 <rayhan.kabir29@gmail.com>
2022-01-04 14:38:09 +0600

    s

--- src/Pages/Choose/Choose.js
+++ src/Pages/Choose/Choose.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Card, Col, Container,Row } from 'react-bootstrap';
+import ATO from '../../images/ATO.png';
+import ASIC from '../../images/ASIC.png';
+import CPA from '../../images/CPA.jpg';
+import ATO2 from '../../images/ATO-2.png';
 
 const Choose = () => {
     return (
@@ -11,12 +15,22 @@ const Choose = () => {
                             <Card.Body>
                                 <Card.Title>Why Choose Global Tax Online?</Card.Title>
                                 <Card.Text>
-                                    <ul>
-                                        <li>Secure online portal</li>
-                                        <li>Access to Expert Accountants</li>
-                                        <li>Affordable, fixed price fee</li>
-                                        <li>Fast lodgements -maximised Refunds</li>
-                                    </ul>
+                                    <Row>
+                                        <ul className='p-5'>
+                                            <li>Secure online portal</li>
+                                            <li>Access to Expert Accountants</li>
+                                            <li>Affordable, fixed price fee</li>
+                                            <li>Fast lodgements -maximised Refunds</li>
+                                        </ul>
+                                    </Row>
+                                    <Row>
+                                        <Col sm={6} md ={6}>
+                                            <img src={ATO} alt="" className='img-fluid'/>
+                                        </Col>
+                                        <Col sm={6} md ={6}>
+                                            <img src={ASIC} alt="" className='img-fluid'/>
+                                        </Col>
+                                    </Row>
                                 </Card.Text>
                             </Card.Body>
                         </Card>
@@ -25,12 +39,22 @@ const Choose = () => {
                    <Card>
                         <Card.Body>
                             <Card.Text>
-                            <ul>
-                                <li>15 Years of Australian Tax and Accounting Experience</li>
-                                <li>Maximize your deductions to maximize your refunds</li>
-                                <li>On spot tax secure Tax lodgments</li>
-                                <li>Visit our conveniently located offices</li>
-                            </ul>
+                                <Row>
+                                    <ul className='p-5'>
+                                    <li>15 Years of Australian Tax and Accounting Experience</li>
+                                    <li>Maximize your deductions to maximize your refunds</li>
+                                    <li>On spot tax secure Tax lodgments</li>
+                                    <li>Visit our conveniently located offices</li>
+                                    </ul>
+                                </Row>
+                                <Row>
+                                    <Col sm={6} md ={6}>
+                                        <img src={CPA} alt="" className='img-fluid' style={{width:'100%'}}/>
+                                    </Col>
+                                    <Col sm={6} md ={6}>
+                                        <img src= {ATO2} alt="" className='img-fluid' style={{width:'45%'}}/>
+                                    </Col>
+                                </Row>
                             </Card.Text>
                             </Card.Body>
                         </Card>
