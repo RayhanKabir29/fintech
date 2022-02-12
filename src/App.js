4c41d3fa5968a09c6c08dee6b935520d7e870fd7
RayhanKabir29 <rayhan.kabir29@gmail.com>
2022-02-12 12:17:28 +0600

    login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch,Route} from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Shared/Home/Home';
@@ -25,6 +26,9 @@ function App() {
           </Route>
           <Route path="/taxcalc">
            <TaxCalculator/>
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
        <Footer/>
